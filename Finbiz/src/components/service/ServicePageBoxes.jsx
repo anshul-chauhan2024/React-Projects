db18ec05c2dbec55d50e888a73ef5a5aa8915b19
anshul-chauhan2024 <anshulchauhan2004@gmail.com>
2025-09-01 10:47:18 +0530

    Updated project files with new changes

--- Finbiz/src/components/service/ServicePageBoxes.jsx
+++ Finbiz/src/components/service/ServicePageBoxes.jsx
@@ -6,12 +6,36 @@ const ServicePageBoxes = () => {
         <div className="mt-20">
             <div className="container">
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <ServiceBox img={images.servPage1} name='Strategy Growth' no='01' />
-                    <ServiceBox img={images.servPage2} name='Grow Business' no='02' />
-                    <ServiceBox img={images.servPage3} name='Proudly Speech' no='03' />
-                    <ServiceBox img={images.servPage2} name='Model Building' no='04' />
-                    <ServiceBox img={images.servPage1} name='Solution Model' no='05' />
-                    <ServiceBox img={images.servPage3} name='Finbiz Solution' no='06' />
+                    <ServiceBox
+                        img={images.servPage1}
+                        name='Strategy Growth'
+                        no='01'
+                    />
+                    <ServiceBox
+                        img={images.servPage2}
+                        name='Grow Business'
+                        no='02'
+                    />
+                    <ServiceBox
+                        img={images.servPage3}
+                        name='Proudly Speech'
+                        no='03'
+                    />
+                    <ServiceBox
+                        img={images.servPage2}
+                        name='Model Building'
+                        no='04'
+                    />
+                    <ServiceBox
+                        img={images.servPage1}
+                        name='Solution Model'
+                        no='05'
+                    />
+                    <ServiceBox
+                        img={images.servPage3}
+                        name='Finbiz Solution'
+                        no='06'
+                    />
                 </div>
             </div>
         </div>
